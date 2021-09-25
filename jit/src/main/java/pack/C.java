03894aad1801fb6d1a21934985d967ebd311da72
cloudsony999 <cloudsony999@gmail.com>
2021-09-25 22:21:54 +0530

    commited

--- jit/src/main/java/pack/C.java
+++ jit/src/main/java/pack/C.java
@@ -1,11 +1,9 @@
 package pack;
 
-class B {
+public class C {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");
-		//gpg --full-generate-key
 
 	}
 
